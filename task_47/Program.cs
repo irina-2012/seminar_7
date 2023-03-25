8e87f65478904774ed7550706d591c7baf90ec77
Irina <pimaki2012@mail.ru>
2023-03-25 14:08:13 +0300

    дз

---
+++ task_47/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+
+Console.Clear();
+GetArray(3,4);
+
+void GetArray(int m, int n)
+{
+    double[,] mas = new double[m, n];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = Convert.ToDouble(new Random().Next(1, 100) / 10.0);
+            Console.Write($"{mas[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ task_50/Program.cs
@@ -0,0 +1,50 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение 
+этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4 */
+
+Console.Clear();
+int[,] mas = GetArray(3, 4);
+
+Console.WriteLine("Введите позиицию элемента: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine(Posihion(mas));
+
+int[,] GetArray(int m, int n)
+{
+    int[,] mas = new int[m, n];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = new Random().Next(1, 100);
+            Console.Write($"{mas[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+    return mas;
+}
+
+
+void Posihion(int[,] mas)
+{
+    // Console.WriteLine("Введите позиицию элемента: ");
+    // int a = int.Parse(Console.ReadLine());
+    // int b = int.Parse(Console.ReadLine());
+    for (int i = 0; i < mas.Length; i++)
+    {
+        for (int j = 0; j < mas.Length; j++)
+        {
+            if (a > m && b > n)
+                Console.WriteLine("такого числа нет");
+            else
+            {
+                object c = array.GetValue(a, b);
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
